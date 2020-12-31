57b0b1f439445511922f742de19dfde0022004a2
qrry <qrry@msn.com>
2021-01-01 06:49:12 +0800

    -fix param

--- plugins/api/control/src/main/java/org/apache/cloudstack/ControlConstants.java
+++ plugins/api/control/src/main/java/org/apache/cloudstack/ControlConstants.java
@@ -8,27 +8,27 @@ public class ControlConstants {
         public static final String UUID = "uuid";
         public static final String DESCRIPTION = "description";
         public static final String ICON = "icon";
-        public static final String RUN_SCRIPT = "runScript";
-        public static final String INSTANCE_COUNT = "instanceCount";
+        public static final String RUN_SCRIPT = "run_script";
+        public static final String INSTANCE_COUNT = "instance_count";
         public static final String REMOVED = "removed";
         public static final String OWNER = "owner";
         public static final String CREATED = "created";
-        public static final String LAST_UPDATED = "lastUpdated";
+        public static final String LAST_UPDATED = "last_updated";
         public static final String STATE = "state";
         public static final String REMARK = "remark";
     }
 
     public static class AppManage{
         public static final String ID = "id";
-        public static final String APP_STORE_ID = "appStoreId";
+        public static final String APP_STORE_ID = "app_store_id";
         public static final String UUID = "uuid";
         public static final String DESCRIPTION = "description";
-        public static final String INSTANCE_ID = "instanceId";
-        public static final String RUN_SCRIPT = "runScript";
+        public static final String INSTANCE_ID = "instance_id";
+        public static final String RUN_SCRIPT = "run_script";
         public static final String REMOVED = "removed";
         public static final String OWNER = "owner";
         public static final String CREATED = "created";
-        public static final String LAST_UPDATED = "lastUpdated";
+        public static final String LAST_UPDATED = "last_updated";
         public static final String STATE = "state";
         public static final String REMARK = "remark";
     }
